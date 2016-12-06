f945045eefab79fd7ada7b5c8a0f2e2349e2e314
Fukuda Kouki <e165727@ie.u-ryukyu.ac.jp>
2016-12-06 09:51:19 +0900

    commit

---
+++ src/main/jp/ac/uryukyu/ie/e165727/ExMain.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e165727;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by Koo on 2016/11/29.
+ */
+public class ExMain {
+
+    BufferedReader reader = new BufferedReader(new FileReader(new File(map.txt)));
+    Player player = new Player();
+
+}
